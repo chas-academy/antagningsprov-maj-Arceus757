c52624922a955fc5fd61db5a62ec2f9e46e573d3
Dmytro <dmytro.malanchuk@elev.ga.ntig.se>
2025-05-08 16:41:06 +0200

    Klar

--- uppg1.js
+++ uppg1.js
@@ -1,6 +1,6 @@
 function uppg1() {
-  // Skriv ut texten "Hello World!" i konsolen
-  console.log("Hello World!");
+   // Skriv ut texten "Hello World!" i konsolen
+   console.log("Hello World!");
 }
   
 module.exports = { uppg1 };
