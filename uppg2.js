bb0da348f90db226dfa427a5f63517e2aaa0ff03
Dmytro <dmytro.malanchuk@elev.ga.ntig.se>
2025-05-09 09:05:24 +0200

    klar

--- uppg2.js
+++ uppg2.js
@@ -2,7 +2,7 @@ function uppg2() {
    // Skriv din kod här
    // deklarera en variable med ditt namn och skriv ut det i konsolen
  
-   const name = "Hampus";
+   const name = "Dmytro";
    console.log(name);
  }
  
